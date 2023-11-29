1ec9d7ae4dbf066f2b9e3b26384e273cd10c0670
Nofrian Deny Hendrawan <nofrian.hendrawan@unmer.ac.id>
2023-11-29 14:36:40 +0700

    Selamat Siang Pak. Ini adalah Tugas UTS saya.

---
+++ src/guti/Guti.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package guti;
+
+/**
+ *
+ * @author USER
+ */
+public class Guti {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String A;
+        int D;
+        float S;
+        float E;
+        float H;
+        
+        A = "Andrew";
+        D = 8;
+        S = 157;
+        E = 35;
+        H = (E/(S/100*S/100));
+        System.out.println("Nama : " + A);
+        System.out.println("Umur : " + D);
+        System.out.println("Tinggi : " + S);
+        System.out.println("Berat : " + E);
+        System.out.println("Hasil BMI : " + H);
+                
+    }
+    
+}
